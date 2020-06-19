a73308c7359d3548fb490ab70ab857782ca142e0
ct <ct@gerudo.local>
2020-06-19 00:27:08 -0400

    cartesian to polar transformation

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ var logger = require('morgan');
 
 var app = express();
 
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(__dirname + '/public'));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,15 +1,59 @@
+
+let time = 0;
+let wave=[];
+let freq = 0.05
 function setup(){
-    createCanvas(windowWidth,windowHeight)
+    createCanvas(600,400)
 }
 
 function draw() {
-    if (mouseIsPressed) {
-    fill(0);
-    } else {
-        fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
+    background(150);
+    translate(200,200)
 
-    console.log("how many frames per second?")
+    ellipse(0,0,3)
+
+    let radius = 50;
+    stroke(255);
+    noFill();
+    ellipse(0,0, radius*2);
+    ellipse(0,0,3)
+    let x = radius * cos(time);
+    let y = radius * sin(time);
+    wave.unshift(y)
+
+    fill(255);
+    ellipse(x,y,12);
+    ellipse(150,y,10)
+
+    for(let i = 0; i < wave.length; i++){
+        point(i+150, wave[i]);
+    }
+    
+
+    // drawText(x,y,wave.length)
+    
+    time += freq;
+    if (wave.length > 100){
+        wave.pop();
+    }
 
 }
+
+function mousePressed() {
+    noLoop();
+  }
+  
+  function mouseReleased() {
+    loop();
+  }
+
+  function drawText(x,y,l){
+    fill(255)
+    stroke(255)
+    textSize(30)
+    text((x),100,100)
+    translate(0,30)
+    text((y),100,100)
+    translate(0,30)
+    text(l,100,100)
+  }
